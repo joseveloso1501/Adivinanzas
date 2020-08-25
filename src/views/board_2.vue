13f996df4ad015602d96b6261950a23fcbeb9610
joseveloso1501 <josevin09@gmail.com>
2020-08-25 05:21:21 -0400

    se agrega slides, puntaje, intentos y blur inicial

--- src/views/board_2.vue
+++ src/views/board_2.vue
@@ -2,76 +2,83 @@
   <v-row justify="space-around">
     <v-col>
       <v-card>
-        <v-row justify="space-around">
-          <v-col>
-            <v-list-item>
-              <v-list-item-content>
-                <v-list-item-title class="space-around"
-                  >Adivina la imagen</v-list-item-title
-                >
-                <v-list-item-subtitle
-                  >Tema: aves endémicas</v-list-item-subtitle
-                >
-              </v-list-item-content>
-            </v-list-item>
-          </v-col>
-        </v-row>
-        <v-card>
+        <v-container fluid>
           <v-row justify="space-around">
-            <v-col justify="center">
-              <div>
-                intentos
-              </div>
-            </v-col>
-            <v-col justify="center">
-              <div>
-                puntaje
-              </div>
-            </v-col>
-            <v-col justify="center">
-              <div>
-                cronometro
-              </div>
+            <v-col>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title class="space-around"
+                    >Adivina la imagen</v-list-item-title
+                  >
+                  <v-list-item-subtitle
+                    >Tema: aves endémicas</v-list-item-subtitle
+                  >
+                </v-list-item-content>
+              </v-list-item>
             </v-col>
           </v-row>
-        </v-card>
-        <v-row justify="center">
-          <v-col>
-            <v-card class="mx-auto" max-width="600">
-              <v-img
-                class="white--text align-end"
-                height="400px"
-                width="578"
-                src="https://t1.ea.ltmcdn.com/es/images/7/0/0/buhos_24007_1_600.jpg"
-              >
-                <!--https://upload.wikimedia.org/wikipedia/commons/7/7e/Enicognathus_leptorhynchus_-two_captive-8a.jpg https://t1.ea.ltmcdn.com/es/images/7/0/0/buhos_24007_1_600.jpg-->
-              </v-img>
-              <!--  <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>  -->
-              <v-card-text class="text--primary">
-                <div>Tu respuesta:</div>
-                <v-text-field :rules="rules"></v-text-field>
-              </v-card-text>
-
-              <v-card-actions>
-                <v-btn color="orange" text>
-                  Comprobar
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-
-          <v-col>
-            <v-row align="start" justify="space-around">
-              <div>
-                <v-btn color="orange" type="submit">
-                  Pedir pista
-                </v-btn>
-              </div>
+          <v-card>
+            <v-row justify="center" align="center">
+              <v-col>
+                <div>{{ intentos }} de 2</div>
+                <div>
+                  INTENTOS
+                </div>
+              </v-col>
+              <v-col>
+                <div id="numPuntos" class="numIndicador">
+                  {{ puntaje }}
+                </div>
+                <div>
+                  PUNTOS
+                </div>
+              </v-col>
+              <v-col>
+                <div>
+                  {{ tiempo }}
+                </div>
+                <div>
+                  TIEMPO
+                </div>
+              </v-col>
             </v-row>
+          </v-card>
+          <v-row justify="center">
+            <v-col>
+              <v-row justify="center">
+                <v-card class="mx-auto">
+                  <v-img
+                    class="blur"
+                    height="400px"
+                    width="578"
+                    src="https://t1.ea.ltmcdn.com/es/images/7/0/0/buhos_24007_1_600.jpg"
+                  >
+                  </v-img>
+                  <v-card-text class="text--primary">
+                    <div>Tu respuesta:</div>
+                    <v-text-field :rules="rules" @click="actualizar"></v-text-field>
+                  </v-card-text>
 
-            <v-row align="center" justify="space-around">
-              <div>
-                <v-sheet class="mx-auto" elevation="3" max-width="800">
+                  <v-card-actions>
+                    <v-btn v-on:click="intento" color="orange" text>
+                      Comprobar
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-row>
+            </v-col>
+
+            <v-col>
+              <v-sheet class="mx-auto" elevation="3" width="650">
+                <v-row align="center" justify="center">
+                  <div>
+                    <v-btn color="orange" type="submit" @click="verPista">
+                      Pedir pista
+                    </v-btn>
+                  </div>
+                </v-row>
+
+                <v-row align="center" justify="space-around">
                   <v-slide-group
                     v-model="model"
                     class="pa-4"
@@ -80,17 +87,17 @@
                     :center-active="centerActive"
                   >
                     <v-slide-item
-                      v-for="n in 5"
+                      v-for="(item, n) in items"
                       :key="n"
-                      v-slot:default="{ active, toggle }"
+                      
                     >
                       <v-card
                         :color="active ? 'primary' : 'grey lighten-1'"
                         class="ma-4"
                         height="150"
                         width="150"
-                        @click="toggle"
-                        >sdfsdfsdf
+                        @click="verPista(n)"
+                      >
                         <v-row
                           class="fill-height"
                           align="center"
@@ -99,30 +106,56 @@
                           <v-scale-transition>
                             <v-icon
                               v-if="active"
+                              align="center"
+                              justify="center"
                               color="white"
-                              size="48"
-                              v-text="aaadasdfshdsnxcbdfa"
+                              size="20"
+                              v-text="item.pista"
                             ></v-icon>
                           </v-scale-transition>
                         </v-row>
                       </v-card>
                     </v-slide-item>
                   </v-slide-group>
-                </v-sheet>
-              </div>
-            </v-row>
-          </v-col>
-
-          <!--<v-card class="mx-auto" max-width="400">
-            <v-container fluid class="pa-0">
-              <v-row align="center">
-                
-              </v-row>
-            </v-container>
-          </v-card>-->
-        </v-row>
+                </v-row>
+                <!--  
+                 <v-container fluid>
+                  <v-row dense>
+                    <v-col v-for="(item, n) in items" :key="n" :cols="4">
+                      <v-card
+                        :color="active ? 'primary' : 'grey lighten-1'"
+                        class="ma-4"
+                        height="150"
+                        width="150"
+                        @click="toggle"
+                        :cols="4"
+                      >
+                        <v-row
+                          class="fill-height"
+                          align="center"
+                          justify="center"
+                        >
+                          <v-scale-transition>
+                            <v-icon
+                              v-if="active"
+                              align="center"
+                              justify="center"
+                              color="white"
+                              size="20"
+                              v-text="item.pista"
+                            ></v-icon>
+                          </v-scale-transition>
+                        </v-row>
+                      </v-card>
+                    </v-col>
+                  </v-row>
+                </v-container>
+                -->
+              </v-sheet>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-card>
-
       <v-row>
         <v-col> </v-col>
       </v-row>
@@ -133,23 +166,64 @@
 export default {
   //el: "#app",
   data: () => ({
-    tiempo: 0,
-    rules: [
-      //value => !!value || 'Required.',
-      //(value) => (value || "").length <= 20 || "Max 20 characters",
-      (value) => {
-        //const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        //return pattern.test(value) || "Invalid answer.";
-      },
+    itemBlur: [
+      { b: "0px" },
+      { b: "5px" },
+      { b: "10px" },
+      { b: "15px" },
+      { b: "20px" },
     ],
+    item: 1,
+    tiempo: 0,
+    active: false,
+
+    rules: [
+      /*
+      value => !!value || 'Required.',
+      (value) => (value || "").length <= 20 || "Max 20 characters",
+      (value) => {
+        const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return pattern.test(value) || "Invalid answer.";
+      },
+      */
+    ],
+
     model: null,
     multiple: true,
     showArrows: true,
     centerActive: false,
     prevIcon: true,
     nextIcon: true,
+    //
+    puntaje: 100,
+    intentos: 0,
+    items: [
+      {
+        pista: "Es de hábitos nocturnos",
+      },
+      {
+        pista: "Puede girar la cabeza en 270°",
+      },
+      {
+        pista: "Tiene ojos muy grandes",
+      },
+      {
+        pista: "Es un ave rapaz",
+      },
+      {
+        pista: "Tiene plumas que parecen orejas",
+      },
+    ],
   }),
   methods: {
+    verPista() {
+      this.active = true;
+      this.puntaje -= 10;
+      
+    },
+    intento() {
+      this.intentos++;
+    },
     aumentar() {
       this.tiempo++;
     },
@@ -159,5 +233,42 @@ export default {
     },
   },
 };
-//app.actualizar();
 </script>
+<style>
+.blur {
+  filter: blur(20px);
+  -webkit-filter: blur(20px);
+  -moz-filter: blur(20px);
+  -o-filter: blur(20px);
+  -ms-filter: blur(20px);
+}
+.blur4 {
+  filter: blur(15px);
+  -webkit-filter: blur(15px);
+  -moz-filter: blur(15px);
+  -o-filter: blur(15px);
+  -ms-filter: blur(15px);
+}
+.blur3 {
+  filter: blur(10);
+  -webkit-filter: blur(20px);
+  -moz-filter: blur(10);
+  -o-filter: blur(10);
+  -ms-filter: blur(10);
+}
+.blur2 {
+  filter: blur(5px);
+  -webkit-filter: blur(5px);
+  -moz-filter: blur(5px);
+  -o-filter: blur(5px);
+  -ms-filter: blur(5px);
+}
+.blur1 {
+  filter: blur(1px);
+  -webkit-filter: blur(1px);
+  -moz-filter: blur(1px);
+  -o-filter: blur(1px);
+  -ms-filter: blur(1px);
+}
+
+</style>
